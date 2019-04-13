e7b2b37567f1c26c9b6b6eebf55d36443440dbab
justin <jlouie10@users.noreply.github.com>
2019-04-13 17:43:25 -0500

    Added interval timer, questions and choices to display, click event to choices

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # TriviaGame
 A trivia game using JavaScript for the logic and jQuery to manipulate HTML.
+
+# Trivia Night: Sports
+
+Description
+
+Other features
+
+# Screenshots
+
+# What's New
+
+0.3
+
+* Added interval timer
+* Display questions and choices on the game screen
+* Added click event to choices
+
+# Instructions
+
+# Issues ([Feature], [Bug])
+
+[Issue] Scope change causes this.initialize to error out in display object
+[Issue] Timeout timer needs to be removed & timer object needs to be refactored
+
+# References
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,13 @@
 $(document).ready(function () {
+  var player;
+  var computer;
+
   var game = {
     status: "start", // start, question, answer, game over
     action: "",
+    time: 5,
+    remaining: 5,
+    guess: 0,
     summary: {
       total: 0,
       correct: 0,
@@ -11,106 +17,80 @@ $(document).ready(function () {
   };
 
   var display = { // Question to TAs: How would one go about simplifying this object, since function calls need to be synchronous with the timer
-    initialize: function() {
+    initialize: function () {
       $("#start").addClass("hide");
       $("#question").addClass("hide");
       $("#answer").addClass("hide");
       $("#game-over").addClass("hide");
     },
-    start: function() {
+    start: function () {
       this.initialize();
       $("#start").removeClass("hide");
     },
-    question: function() {
+    question: function (text, array) {
       // this.initialize();
 
       // Fix for scope change
       $("#start").addClass("hide");
-      $("#question").addClass("hide");
       $("#answer").addClass("hide");
       $("#game-over").addClass("hide");
 
       $("#question").removeClass("hide");
+      var questionDiv = $("<div>");
+      var i;
+
+      questionDiv.text(text);
+      $("#question-container").html(questionDiv)
+
+      for (i = 0; i < array.length; i++) {
+        var choiceDiv = $("<div>");
+
+        choiceDiv.text(array[i])
+        choiceDiv.addClass("choice");
+        choiceDiv.attr("data-value", i);
+
+        $("#choice-container").append(choiceDiv);
+      }
     },
-    answer: function() {
+    answer: function () {
       // this.initialize();
 
       // Fix for scope change
       $("#start").addClass("hide");
       $("#question").addClass("hide");
-      $("#answer").addClass("hide");
       $("#game-over").addClass("hide");
 
       $("#answer").removeClass("hide");
     },
-    gameOver: function() {
+    gameOver: function () {
       // this.initialize();
 
       // Fix for scope change
       $("#start").addClass("hide");
       $("#question").addClass("hide");
       $("#answer").addClass("hide");
-      $("#game-over").addClass("hide");
 
       $("#game-over").removeClass("hide");
+    },
+    updateTimer: function (number) {
+      $("#question-timer").html("<span>" + number + "</span>")
     }
   };
 
   var trivia = {
-    version: 1,
-    items: {
-      first: {
-        id: 0,
-        question: "",
-        answer: "",
-        choices: [
-          "Question " + this.id + ", choice a",
-          "Question " + this.id + ", choice b",
-          "Question " + this.id + ", choice c",
-          "Question " + this.id + ", choice d"
-        ],
-        asked: false
-      },
-      second: {
-        id: 1,
-        question: "",
-        answer: "",
-        choices: [
-          "Question " + this.id + ", choice a",
-          "Question " + this.id + ", choice b",
-          "Question " + this.id + ", choice c",
-          "Question " + this.id + ", choice d"
-        ]
-      },
-      third: {
-        id: 2,
-        question: "",
-        answer: "",
-        choices: [
-          "Question " + this.id + ", choice a",
-          "Question " + this.id + ", choice b",
-          "Question " + this.id + ", choice c",
-          "Question " + this.id + ", choice d"
-        ],
-        asked: false
-      },
-      fourth: {
-        id: 3,
-        question: "",
-        answer: "",
-        choices: [
-          "Question " + this.id + ", choice a",
-          "Question " + this.id + ", choice b",
-          "Question " + this.id + ", choice c",
-          "Question " + this.id + ", choice d"
-        ],
-        asked: false
-      },
-    }
+    version: 0,
+    question: "Michael Jordan played college basketball at which university?",
+    choices: [
+      "UCLA", 
+      "Kentucky", 
+      "Duke", 
+      "North Carolina"
+    ]
   };
 
   var timer = {
     question: {
+      number: 5,
       set: 0,
       start: function (newDisplay) {
         this.set = setTimeout(newDisplay, 5000);
@@ -141,14 +121,59 @@ $(document).ready(function () {
   }
 
   $(document).on("click", "#start-btn", start);
+  $(document).on("click", ".choice", choose);
 
   function start() {
     game.action = "start";
-    display.question();
+    display.question(trivia.question, trivia.choices);
+    display.updateTimer(game.remaining); // Initialize the countdown timer
 
     // Start question timer, change display if timer expires
-    timer.question.start(display.answer);
+    // timer.question.start(display.answer);
+
+    run();
 
     hooks.update(game.action);
   }
+
+  function choose() {
+    userGuess = $(this).attr("data-value");
+
+    stop();
+
+    game.action = "guess: " + userGuess;
+    game.remaining = game.time;
+    display.answer();
+
+    hooks.update(game.action);
+  }
+
+  function run() {
+    player = setInterval(countdown, 1000);
+  }
+
+  function stop() {
+    clearInterval(player);
+  }
+
+  function countdown() {
+    game.remaining--;
+
+    if (game.remaining === 0) {
+      stop();
+
+      game.action = "time expired";
+      game.remaining = game.time;
+      display.answer();
+
+      hooks.update(game.action);
+    }
+
+    display.updateTimer(game.remaining);
+  }
+
+  function random() {
+    return 0; //this.pack[Math.floor(Math.random() * this.pack.length)];
+  }
+
 });
